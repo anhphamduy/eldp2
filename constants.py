c8f4624a5f46990e96098ea8c8e65c9f8070d492
Anh Pham <anhphamduy@outlook.com>
2019-09-16 09:47:02 +1000

    Add file paths

---
+++ constants.py
@@ -0,0 +1,15 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+GOOGLE_PATH = os.path.join(dir_path, 'google.csv')
+AMAZON_PATH = os.path.join(dir_path, 'amazon.csv')
+
+GOOGLE_SMALL_PATH = os.path.join(dir_path, 'google_small.csv')
+AMAZON_SMALL_PATH = os.path.join(dir_path, 'amazon_small.csv')
+
+
+GOOGLE_AMAZON_SMALL_LABEL_PATH = os.path.join(
+    dir_path, 'amazon_google_truth_small.csv')
+GOOGLE_AMAZON_LABEL_PATH = os.path.join(
+    dir_path, 'amazon_google_truth.csv')
